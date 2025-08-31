7fa16a37868fd5dffc8dc84fd0c60f631e1d950c
garfazeon <derevyankin2004@mail.ru>
2025-09-01 01:15:43 +0900

    create a web todo

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit import text_input
 
 import functions
 todos = functions.get_todos()
@@ -12,9 +11,12 @@ def add_todo():
 st.title('My to-do App')
 st.subheader('This is my todo app')
 
-for todo in todos:
-    st.checkbox(todo)
-
+for index,todo in enumerate(todos):
+    checkbox =st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 st.text_input(label='',placeholder='Add a new todo...',
               on_change=add_todo,key='new_todo')
-
